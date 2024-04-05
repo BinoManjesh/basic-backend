e274a4e4dfa10f757eb9a727727652d13f3d00a4
BinoManjesh <binomanjesh@gmail.com>
2024-04-05 17:46:16 +0530

    Send file according to url

--- index.js
+++ index.js
@@ -6,10 +6,29 @@ async function writeFile(res, fileName) {
   res.write(file);
 }
 
+function getFileName(url) {
+  switch (url) {
+    case "/":
+      return "index.html";
+    case "/about":
+      return "about.html";
+    case "/contact-me":
+      return "contact-me.html";
+    default:
+      return false;
+  }
+}
+
 http
   .createServer(async function (req, res) {
-    res.writeHead(200, { "Content-type": "text/html" });
-    await writeFile(res, "index.html");
+    let fileName = getFileName(req.url);
+    if (fileName) {
+      res.writeHead(200, { "Content-type": "text/html" });
+    } else {
+      res.writeHead(404, { "Content-type": "text/html" });
+      fileName = "404.html";
+    }
+    await writeFile(res, fileName);
     res.end();
   })
   .listen(8080, () => {
