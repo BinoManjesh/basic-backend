3a825b2999f8a566f0dec5f79c33494ecbc1b9b8
BinoManjesh <binomanjesh@gmail.com>
2024-04-05 17:34:28 +0530

    Send index.html

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const http = require("node:http");
 const fs = require("node:fs/promises");
 
+async function writeFile(res, fileName) {
+  const file = await fs.readFile(fileName);
+  res.write(file);
+}
+
 http
   .createServer(async function (req, res) {
-    res.writeHead(200);
+    res.writeHead(200, { "Content-type": "text/html" });
+    await writeFile(res, "index.html");
     res.end();
   })
   .listen(8080, () => {
